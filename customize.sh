ace418d0d228fa3ce0f63ecf0a4ebb0889ea746b
tycn888 <77333646+tycn888@users.noreply.github.com>
2023-01-02 15:48:24 +0800

    Update customize.sh

--- customize.sh
+++ customize.sh
@@ -12,4 +12,4 @@ sed -i 's/192.168.1.1/192.168.2.20/g' openwrt/package/base-files/files/bin/confi
 sed -i 's/$1$V4UetPzk$CYXluq4wUazHjmCDBCqXF.//g' openwrt/package/lean/default-settings/files/zzz-default-settings
 
 #3. Replace with JerryKuKu’s Argon
-#rm openwrt/package/lean/luci-theme-argon -rf
+rm openwrt/package/lean/luci-theme-argon -rf
